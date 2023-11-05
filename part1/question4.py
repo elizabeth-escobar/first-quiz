a6b746801879ddf068ae38236add7ea23a17d33b
elizabeth-escobar <149844965+elizabeth-escobar@users.noreply.github.com>
2023-11-04 19:43:28 -0500

    Update question4.py

--- part1/question4.py
+++ part1/question4.py
@@ -7,7 +7,22 @@ import pets_db
 #  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /       /__  __/
 #  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/          /_/   
 #                                                                          
-#  Question 4
+#  Question 4the pets that are owned by nobody.
+# The output should be a list of tuples in the format: (<pet name>, <species>, <age>)
+
+sql_pets_owned_by_nobody = """
+SELECT pet.name, pet.species, pet.age
+FROM animals pet
+LEFT JOIN people_animals pa ON pet.animal_id = pa.pet_id
+WHERE pa.owner_id IS NULL;
+"""
+
+# Part 4.B:
+# Write SQL to select how the number of pets are older than their owners. 
+# The output should be an integer.
+
+sql_pets_older_than_owner = """
+
 ################################################################################
 #
 # Instructions:
@@ -19,22 +34,12 @@ import pets_db
 # is related.
 
 # Part 4.A:
-# Write SQL to select the pets that are owned by nobody.
-# The output should be a list of tuples in the format: (<pet name>, <species>, <age>)
-
-sql_pets_owned_by_nobody = """
-
-Your SQL here.
-
-"""
-
-# Part 4.B:
-# Write SQL to select how the number of pets are older than their owners. 
-# The output should be an integer.
-
-sql_pets_older_than_owner = """
-
-Your SQL here.
+# Write SQL to select 
+SELECT COUNT(*) 
+FROM animals pet
+JOIN people_animals pa ON pet.animal_id = pa.pet_id
+JOIN people p ON pa.owner_id = p.person_id
+WHERE pet.age > p.age;
 
 """
 
@@ -43,6 +48,10 @@ Your SQL here.
 # The output should be a list of tuples in the format: (<person name>, <pet name>, <species>)
 sql_only_owned_by_bessie = """ 
 
-Your SQL here.
-
+SELECT p.name AS person_name, pet.name AS pet_name, pet.species
+FROM animals pet
+JOIN people_animals pa ON pet.animal_id = pa.pet_id
+JOIN people p ON pa.owner_id = p.person_id
+WHERE p.name = 'bessie'
+AND (SELECT COUNT(*) FROM people_animals pa2 WHERE pa2.pet_id = pa.pet_id) = 1;
 """
