4698c0b9f50c52cbf209005d3cce94e298117dd8
elizabeth-escobar <149844965+elizabeth-escobar@users.noreply.github.com>
2023-11-05 11:08:06 -0500

    Update VendingMachineImpl.java

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,57 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+public class VendingMachineImpl implements VendingMachine {
+
+  private int quartersInserted;
+  private boolean drinkDispensed;
+
+  private static VendingMachineImpl instance;
+
+  private VendingMachineImpl() {
+    this.quartersInserted = 0;
+    this.drinkDispensed = false;
+  }
 
   public static VendingMachine getInstance() {
     // Fix me!
-    return null;
+  if (instance == null) {
+      instance = new VendingMachineImpl();
+    }
+    return instance;
+  }
+
+  public void insertQuarter() {
+    quartersInserted++;
+  }
+
+    public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+    if (name.equals("ScottCola")) {
+      if (quartersInserted >= 3) {
+        quartersInserted -= 3;
+        drinkDispensed = true;
+        return new ScottCola();
+      } else {
+        throw new NotEnoughMoneyException();
+      }
+    } else if (name.equals("KarenTea")) {
+      if (quartersInserted >= 4) {
+        quartersInserted -= 4;
+        drinkDispensed = true;
+        return new KarenTea();
+      } else {
+        throw new NotEnoughMoneyException();
+      }
+    } else {
+      throw new UnknownDrinkException();
+    }
+  }
+
+  public boolean isDrinkDispensed() {
+    return drinkDispensed;
+  }
+
+  public void reset() {
+    quartersInserted = 0;
+    drinkDispensed = false;
   }
 }
